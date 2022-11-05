ca038b5fdcd530566e4480bd3cc97859c2711cbb
TrongTin <trongtin3311@gmail.com>
2022-11-05 17:12:16 +0700

    fix dangky

--- src/pages/SignUp/Signup.tsx
+++ src/pages/SignUp/Signup.tsx
@@ -43,7 +43,7 @@ export default function Signup({}: Props) {
     onSubmit: (values:any) => {
         console.log(values);
         dispatch(signupApi(values))
-        navigate("/login")
+        navigate("/login") 
     }
 })
 const handleChangeDatePicker=(value:any)=>{
